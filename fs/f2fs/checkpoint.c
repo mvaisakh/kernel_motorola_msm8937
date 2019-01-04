166c20de90023c8493b12be5b0592cd4cee0ee80
Alberto97 <albertop2197@gmail.com>
2019-01-04 23:01:50 +0100

    Revert "mm: remove nr_pages argument from pagevec_lookup_{,range}_tag()"
    This reverts commit 767fcf3323c4c38628fb4c7933f9c843efc16766 except fs/f2fs/node.c
    
    Signed-off-by: Vaisakh Murali <vaisakhmurali@gmail.com>

--- fs/f2fs/checkpoint.c
+++ fs/f2fs/checkpoint.c
@@ -322,13 +322,9 @@ long sync_meta_pages(struct f2fs_sb_info *sbi, enum page_type type,
 
 	blk_start_plug(&plug);
 
-	while (index <= end) {
-		int i, nr_pages;
-		nr_pages = pagevec_lookup_tag(&pvec, mapping, &index,
-				PAGECACHE_TAG_DIRTY,
-				min(end - index, (pgoff_t)PAGEVEC_SIZE-1) + 1);
-		if (unlikely(nr_pages == 0))
-			break;
+	while ((nr_pages = pagevec_lookup_tag(&pvec, mapping, &index,
+				PAGECACHE_TAG_DIRTY, PAGEVEC_SIZE))) {
+		int i;
 
 		for (i = 0; i < nr_pages; i++) {
 			struct page *page = pvec.pages[i];
