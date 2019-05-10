25978acb459eb2607ae60f6c9aad3fc33465376c
Tiago Ganselli <ganselli@motorola.com>
2019-05-10 18:49:34 +0200

    Comment out the non-QHMP dependency
    Broke Compilation, also unneeded on msm8937 devices
    
    Propagated from (CR)
    
    Signed-off-by: Vaisakh Murali <vaisakhmurali@gmail.com>
    Change-Id: I7a23b715839cccc9f72774dc35535dc569cf3422
    Signed-off-by: Tiago Ganselli <ganselli@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1246266
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Author: Tiago Ganselli <ganselli@motorola.com>
    Tested-by: Jira Key
    Reviewed-by: Ling Jin <lingjin@motorola.com>
    Submit-Approved: Jira Key

--- kernel/sched/qhmp_sched.h
+++ kernel/sched/qhmp_sched.h
@@ -1232,16 +1232,15 @@ static inline bool task_notify_on_migrate(struct task_struct *p)
 static inline void __set_task_cpu(struct task_struct *p, unsigned int cpu)
 {
 	set_task_rq(p, cpu);
-#ifdef CONFIG_SMP
-	/*
+/*#ifdef CONFIG_SMP
+
 	 * After ->cpu is set up to a new value, task_rq_lock(p, ...) can be
 	 * successfuly executed on another CPU. We must ensure that updates of
 	 * per-task data have been completed by this moment.
-	 */
 	smp_wmb();
 	task_thread_info(p)->cpu = cpu;
 	p->wake_cpu = cpu;
-#endif
+#endif */
 }
 
 /*
