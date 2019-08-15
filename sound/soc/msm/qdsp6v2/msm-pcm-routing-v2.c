MotorolaMobilityLLC/kernel-msm

ASoC: msm: qdsp6v2: Fix VOC_EXT_EC MUX get control
VOC_EXT_EC MUX get control doesn't convert actual port number to
ENUM value which makes get control useless. By converting it to
ENUM number, get control can present what is set correctly.

Change-Id: I1bab169c0465964f917959ff9399075a823a05c1
Signed-off-by: SeungYoon Lee <w22905@motorola.com>
Reviewed-on: http://gerrit.mot.com/825410
SME-Granted: SME Approvals Granted
SLTApproved: Slta Waiver <sltawvr@motorola.com>
Tested-by: Jira Key <jirakey@motorola.com>
Reviewed-by: Joseph Dwyer <josephdwyer@motorola.com>
Reviewed-by: Igor Kovalenko <igork@motorola.com>
Submit-Approved: Jira Key <jirakey@motorola.com>

sound/soc/msm/qdsp6v2/msm-pcm-routing-v2.c
@@ -2038,7 +2038,26 @@ static int msm_routing_ext_ec_get(struct snd_kcontrol *kcontrol,
 	pr_debug("%s: ext_ec_ref_rx  = %x\n", __func__, msm_route_ext_ec_ref);
 
 	mutex_lock(&routing_lock);
-	ucontrol->value.integer.value[0] = msm_route_ext_ec_ref;
+	switch (msm_route_ext_ec_ref) {
+	case AFE_PORT_ID_PRIMARY_MI2S_TX:
+		ucontrol->value.integer.value[0] = EC_PORT_ID_PRIMARY_MI2S_TX;
+		break;
+	case AFE_PORT_ID_SECONDARY_MI2S_TX:
+		ucontrol->value.integer.value[0] = EC_PORT_ID_SECONDARY_MI2S_TX;
+		break;
+	case AFE_PORT_ID_TERTIARY_MI2S_TX:
+		ucontrol->value.integer.value[0] = EC_PORT_ID_TERTIARY_MI2S_TX;
+		break;
+	case AFE_PORT_ID_QUATERNARY_MI2S_TX:
+		ucontrol->value.integer.value[0] = EC_PORT_ID_QUATERNARY_MI2S_TX;
+		break;
+	case SLIMBUS_1_TX:
+		ucontrol->value.integer.value[0] = EC_PORT_ID_SLIMBUS_1_TX;
+		break;
+	default:
+		ucontrol->value.integer.value[0] = 0; /* NONE */
+		break;
+	}
 	mutex_unlock(&routing_lock);
 	return 0;
 }

mvaisakh/kernel_motorola_msm8937

Signed-off-by: Vaisakh Murali <vaisakhmurali@gmail.com>

sound/soc/msm/qdsp6v2/msm-pcm-routing-v2.c
@@ -3293,7 +3293,26 @@ static int msm_routing_ext_ec_get(struct snd_kcontrol *kcontrol,
 	pr_debug("%s: ext_ec_ref_rx  = %x\n", __func__, msm_route_ext_ec_ref);
 
 	mutex_lock(&routing_lock);
-	ucontrol->value.integer.value[0] = msm_route_ext_ec_ref;
+	switch (msm_route_ext_ec_ref) {
+	case AFE_PORT_ID_PRIMARY_MI2S_TX:
+		ucontrol->value.integer.value[0] = EXT_EC_REF_PRI_MI2S_TX;
+		break;
+	case AFE_PORT_ID_SECONDARY_MI2S_TX:
+		ucontrol->value.integer.value[0] = EXT_EC_REF_SEC_MI2S_TX;
+		break;
+	case AFE_PORT_ID_TERTIARY_MI2S_TX:
+		ucontrol->value.integer.value[0] = EXT_EC_REF_TERT_MI2S_TX;
+		break;
+	case AFE_PORT_ID_QUATERNARY_MI2S_TX:
+		ucontrol->value.integer.value[0] = EXT_EC_REF_QUAT_MI2S_TX;
+		break;
+	case SLIMBUS_1_TX:
+		ucontrol->value.integer.value[0] = EXT_EC_REF_SLIM_1_TX;
+		break;
+	default:
+		ucontrol->value.integer.value[0] = 0; /* NONE */
+		break;
+	}
 	mutex_unlock(&routing_lock);
 	return 0;
 }